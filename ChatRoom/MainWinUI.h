848eb5c7b62b7e643afacf202b9728e7081c5e9e
BarnabyRoss <chentong0547@outlook.com>
2022-01-04 12:29:51 +0800

    MainWinUI类初步设计

--- ChatRoom/MainWinUI.h
+++ ChatRoom/MainWinUI.h
@@ -2,6 +2,14 @@
 #define __MAINWINUI_H__
 
 #include <QWidget>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QGridLayout>
+#include <QGroupBox>
+#include <QPlainTextEdit>
+#include <QLineEdit>
+#include <QLabel>
+#include <QPushButton>
 
 class MainWinUI : public QWidget{
 
@@ -10,5 +18,20 @@ class MainWinUI : public QWidget{
 public:
   MainWinUI(QWidget* parent = nullptr);
   ~MainWinUI();
+
+private:
+  QVBoxLayout m_vLayout;
+  QHBoxLayout m_hLayout;
+  QGridLayout m_gridLayout;
+
+  QGroupBox m_msgGrpBx;
+  QGroupBox m_inputGrpBx;
+
+  QPlainTextEdit m_msgEditor;
+  QLineEdit m_inputEdit;
+  QLabel m_statusLbl;
+  QPushButton m_logInOutBtn;
+  QPushButton m_sendBtn;
+
 };
 #endif // __MAINWINUI_H__
