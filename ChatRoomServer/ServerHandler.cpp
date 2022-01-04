a0155aada1ff229a308c43bd617732ffdb063073
BarnabyRoss <chentong0547@outlook.com>
2022-01-04 23:24:23 +0800

    ServerHandler类进一步实现-业务逻辑框架初步实现

--- ChatRoomServer/ServerHandler.cpp
+++ ChatRoomServer/ServerHandler.cpp
@@ -11,4 +11,12 @@ void ServerHandler::handle(QTcpSocket& tcp, TextMessage& message){
   qDebug() << message.type();
   qDebug() << message.length();
   qDebug() << message.data();
+
+  if( message.type() == "CONN" ){
+
+
+  }else if( message.type() == "DSNN" ){
+
+
+  }
 }
--- ChatRoomServer/ServerHandler.h
+++ ChatRoomServer/ServerHandler.h
@@ -2,6 +2,7 @@
 #define __SERVERHANDLER_H__
 
 #include <QTcpSocket>
+#include <QList>
 #include "TextMsgHandler.h"
 #include "TextMessage.h"
 
@@ -12,6 +13,14 @@ public:
 
   virtual void handle(QTcpSocket& tcp, TextMessage& message);
 
+private:
+  struct Node{
+
+    QString usr;
+    QString pwd;
+    QTcpSocket* socket;
+  };
+  QList<Node> m_nodeList;
 };
 
 #endif // __SERVERHANDLER_H__
